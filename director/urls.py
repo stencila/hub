e03f67e114db34e28c893b3c64cb013271be9878
Nokome Bentley <nokome@stenci.la>
2015-11-30 11:20:07 +1300

    Merge separate API urls into root URLs

--- director/urls.py
+++ director/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 import os
+import json
 
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core import urlresolvers
 
 import general.views
 import components.views
@@ -41,7 +43,7 @@ urlpatterns = [
     url(r'^me/signup/?$',                                            users.views.signup),
     url(r'^me/signin/?$',                                            users.views.signin),
     url(r'^me/signout/?$',                                           users.views.signout),
-    url(r'^me/',                                                     include('allauth.urls')),
+    url(r'^me/?$',                                                   users.views.me_read),
     # The allauth URLs not overidden above are:
         # password/change/                    account_change_password         change password
         # password/set/                       account_set_password            confirmation that password is changed?
@@ -57,6 +59,12 @@ urlpatterns = [
         # social/login/error/                 socialaccount_login_error
         # social/connections                  socialaccount_connections
 
+    url(r'^tokens/?$',                                               users.views.tokens),
+    url(r'^tokens/(?P<id>\d+)/?$',                                   users.views.tokens),
+
+    url(r'^users/?$',                                                users.views.users_list),
+    url(r'^users/(?P<username>[\w-]+)/?$',                           users.views.users_read),
+
     # Administration interface
     url(r'^admin/',                                                  admin.site.urls),
 
@@ -69,8 +77,10 @@ urlpatterns = [
 
 # In development, serve static files that are handled by `nginx.conf` when in production
 if settings.MODE == 'local':
+
     # Dynamically generated content
     urlpatterns += static(r'/dynamic/', document_root=os.path.join(settings.BASE_DIR, 'dynamic'))
+
     # Javascript (and potentially other resources) on get.stenci.la when in production
     # cal be obtained from a local build via a symlink
     if settings.GET_LOCAL:
@@ -79,6 +89,58 @@ if settings.MODE == 'local':
         urlpatterns += [
             url(r'^get/(?P<path>.*)$', lambda request, path: HttpResponseRedirect('https://s3-us-west-2.amazonaws.com/get.stenci.la/'+path))
         ]
+
+    # Django Debug Toolbar for JSON API endpoints
+    # Useful for checking performance of views, in particular
+    # the number of database hits
+    #
+    # From https://gist.github.com/marteinn/5693665
+
+    def html_decorator(func):
+        """
+        This decorator wraps the JSON API output in HTML.
+        (From http://stackoverflow.com/a/14647943)
+        """
+
+        def _decorated(*args, **kwargs):
+            response = func(*args, **kwargs)
+
+            # Attempt to pretty print JSON
+            try:
+                content = json.loads(response.content)
+                content = json.dumps(content, indent=4, separators=(',', ': '))
+            except:
+                content = response.content
+
+            wrapped = ("<html><body><pre>",
+                       content,
+                       "</pre></body></html>")
+
+            return HttpResponse(wrapped)
+
+        return _decorated
+
+    @html_decorator
+    def debug(request):
+        """
+        Debug endpoint that uses the html_decorator,
+        """
+        path = request.META.get("PATH_INFO")
+        api_url = path.replace("/debug", "")
+
+        view = urlresolvers.resolve(api_url)
+
+        accept = request.META.get("HTTP_ACCEPT")
+        accept += ",application/json"
+        request.META["HTTP_ACCEPT"] = accept
+
+        res = view.func(request, **view.kwargs)
+        return HttpResponse(res._container)
+
+    urlpatterns += [
+        url(r'^.+/debug', debug),
+    ]
+
 
 urlpatterns += [
 
