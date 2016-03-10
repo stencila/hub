e9b3e273ab759a8de4761af318a15a739d6d2138
Nokome Bentley <nokome@stenci.la>
2016-03-10 19:42:27 +1300

    Add missing import

--- director/sessions_/views.py
+++ director/sessions_/views.py
@@ -2,6 +2,7 @@ from django.views.decorators.http import require_http_methods, require_GET, requ
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 from general.authentication import require_authenticated
 from general.api import API
