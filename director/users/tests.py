f8d7d683993267de9dfd9b334d6e47abd66678d2
Nokome Bentley <nokome@stenci.la>
2016-03-22 18:10:16 +1300

    Change to GNU Affero GPL; add copyright and permission statement to files
    The GNU Affero GPL is more applicable than the GNU GPL given this is
    software will usually be run over the network.

--- director/users/tests.py
+++ director/users/tests.py
@@ -1,3 +1,20 @@
+#  This file is part of Stencila Hub.
+#  
+#  Copyright (C) 2015-2016 Stencila Ltd.
+#  
+#  Stencila Hub is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  
+#  Stencila Hub is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#  
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Stencila Hub.  If not, see <http://www.gnu.org/licenses/>.
+
 from django.test import TestCase
 from django.core.exceptions import PermissionDenied
 
