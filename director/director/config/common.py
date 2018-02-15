43098ae1e53e30c98f5882753f43089f64effb29
Richard Mansfield <richard@dragonfly.co.nz>
2018-02-15 13:53:24 +1300

    Add provider settings back in

--- director/director/config/common.py
+++ director/director/config/common.py
@@ -8,6 +8,9 @@ from configurations import Configuration, values
 BASE_DIR = dirname(dirname(dirname(__file__)))
 
 class Common(Configuration):
+    ADMINS = (
+        ('Nokome Bentley', 'nokome@stenci.la'),
+    )
 
     ALLOWED_HOSTS = ['*']
     SECRET_KEY = 'changeme'
@@ -113,4 +116,61 @@ class Common(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
     STATIC_URL = '/static/'
-    
+
+    SITE_ID = 1
+    AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+        'allauth.account.auth_backends.AuthenticationBackend',
+    )
+
+    ACCOUNT_SESSION_REMEMBER = True # Always remember the user
+    ACCOUNT_SESSION_COOKIE_AGE = 31536000 # Sessions to last up to a year 60*60*24*365
+    SOCIALACCOUNT_ADAPTER = 'general.allauth_adapter.SocialAccountAdapter'
+    ACCOUNT_EMAIL_REQUIRED = True
+    SOCIALACCOUNT_QUERY_EMAIL = True
+    SOCIALACCOUNT_PROVIDERS = {
+        'facebook': {
+            # See https://github.com/pennersr/django-allauth#facebook
+            # Manage the app at http://developers.facebook.com logged in as user `stencila`
+            # Callback URL must be HTTP
+            'SCOPE': ['email'],
+            'METHOD': 'oauth2',
+        },
+        'github': {
+            # See http://developer.github.com/v3/oauth/#scopes for list of scopes available
+            # At the time of writing it was not clear if scopes are implemented in allauth for Github
+            # see https://github.com/pennersr/django-allauth/issues/369
+            # Manage the app at https://github.com/organizations/stencila/settings/applications/74505
+            # Callback URL must be HTTP
+            'SCOPE': ['user:email']
+        },
+        'google': {
+            # Manage the app at
+            #   https://code.google.com/apis/console/
+            #   https://cloud.google.com/console/project/582496091484/apiui/credential
+            #   https://cloud.google.com/console/project/582496091484/apiui/consent
+            'SCOPE': ['profile', 'email'],
+            'AUTH_PARAMS': {'access_type': 'online'}
+        },
+        'linkedin': {
+            # Manage the app at
+            #  https://www.linkedin.com/developer/apps/3129843/auth
+            # logged in as a user with access rights to the app
+            # The scopes are listed on the above page
+            'SCOPE': ['r_fullprofile', 'r_emailaddress'],
+            'PROFILE_FIELDS': [
+                'id',
+                'first-name',
+                'last-name',
+                'email-address',
+                'picture-url',
+                'public-profile-url'
+            ]
+        },
+        'orcid': {
+            # Manage the app at https://orcid.org/developer-tools logged in as 0000-0003-1608-7967 (Nokome Bentley)
+        },
+        'twitter': {
+            # Manage the app at https://dev.twitter.com/apps/5640979/show logged in as user `stencila`
+        },
+    }
