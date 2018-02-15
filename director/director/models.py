d41aea1a05b51afe98e96d566fd22ab4d197a541
Richard Mansfield <richard@dragonfly.co.nz>
2018-02-16 11:14:49 +1300

    Add cluster model

--- director/director/models.py
+++ director/director/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+import jwt
+
 class Project(models.Model):
     address = models.TextField()
     gallery = models.BooleanField(default=False)
     users = models.ManyToManyField('auth.User', related_name='projects')
+
+    class Meta:
+        app_label = 'director'
+
+class Cluster(models.Model):
+    host = models.TextField()
+    secret = models.TextField()
+
+    def get_jwt(self, payload):
+        return jwt.encode(payload, self.secret, algorithm='HS256')
 
     class Meta:
         app_label = 'director'
