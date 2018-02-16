fc371eaf1face7a3a405094d79b20f5315e72073
Finlay Thompson <finlay@dragonfly.co.nz>
2018-02-17 12:14:33 +1300

    Allow STATIC_URL to be overridden

--- director/director/config/common.py
+++ director/director/config/common.py
@@ -120,7 +120,7 @@ class Common(Configuration):
 
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    STATIC_URL = '/static/'
+    STATIC_URL = values.Value('/static/')
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'client')
     ]
