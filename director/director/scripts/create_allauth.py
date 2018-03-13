afb75c7e3c14b3f3c4358fbdbea83d4e8f238c91
Nokome Bentley <nokome@stenci.la>
2018-03-13 13:21:40 +1300

    Use scripts for dynamic, instead of static, dev fixtures

---
+++ director/director/scripts/create_allauth.py
@@ -0,0 +1,43 @@
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialApp
+import json
+import os
+import simplecrypt
+import warnings
+
+
+def run():
+    # There must be at least one site
+    site, created = Site.objects.get_or_create(pk=1)
+    site.domain = 'stenci.la'
+    site.name = 'stenci.la'
+    site.save()
+
+    # Get oauth client ids and secrets
+    try:
+        path = os.path.join(
+            os.path.dirname(__file__), '..', '..', '..',
+            'secrets', 'director-allauth.json.enc'
+        )
+        pw = os.environ['STENCILA_DEVPASS']
+        with open(path, 'rb') as file:
+            data = simplecrypt.decrypt(pw, file.read())
+            secrets = json.loads(data)
+    except Exception as exc:
+        warnings.warn('Warning, unable to open allauth SocialApp secrets:', RuntimeWarning)
+        print(exc)
+        secrets = {}
+
+    # Create `SocialApp` objects for each provider
+    for provider in 'facebook', 'github', 'google', 'linkedin', 'twitter':
+        provider_secrets = secrets.get(provider, {
+            'client_id': 'missing-client-id',
+            'secret': 'missing-secret'
+        })
+        app = SocialApp.objects.create(
+            provider=provider,
+            name=provider,
+            client_id=provider_secrets['client_id'],
+            secret=provider_secrets['secret'],
+        )
+        app.sites.add(1)
