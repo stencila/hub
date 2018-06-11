3a267020b1883264711bd4e7dbcca3835d68a26d
Richard Mansfield <richard@dragonfly.co.nz>
2018-06-12 10:06:25 +1200

    Example AWS credentials

--- director/director/config/dev.py
+++ director/director/config/dev.py
@@ -13,3 +13,7 @@ class Dev(Common):
     ]
 
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    AWS_ACCESS_KEY_ID = 'AAA'
+    AWS_SECRET_ACCESS_KEY = 'abc123'
+    AWS_STORAGE_BUCKET_NAME = 'test.bucket.stenci.la'
--- director/director/config/prod.py
+++ director/director/config/prod.py
@@ -10,3 +10,7 @@ class Prod(Common):
     JWT_SECRET = values.Value(None, environ_required=True)
 
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    AWS_ACCESS_KEY_ID = 'AAA'
+    AWS_SECRET_ACCESS_KEY = 'abc123'
+    AWS_STORAGE_BUCKET_NAME = 'bucket.stenci.la'
