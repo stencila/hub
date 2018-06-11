97c573a54c0b3198601dc684541f4daa3bb8becd
Richard Mansfield <richard@dragonfly.co.nz>
2018-06-12 10:08:56 +1200

    Commit non-working google drive stuff

--- director/director/storer/dropbox.py
+++ director/director/storer/dropbox.py
@@ -4,6 +4,8 @@ from . import Storer
 class DropboxStorer(Storer):
     code = 'dropbox'
     name = 'Dropbox'
+    unit_name = 'public folder'
+    unit_name_plural = 'public folders'
 
     def parse_path(self, path):
         self.path = path
