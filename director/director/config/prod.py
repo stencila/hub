57cbafbfa8482ff29921afb9e2e9ac9b7d010080
Nokome Bentley <nokome@stenci.la>
2018-06-15 15:16:11 +1200

    Add Sentry404CatchMiddleware

--- director/director/config/prod.py
+++ director/director/config/prod.py
@@ -11,6 +11,11 @@ class Prod(Common):
         'raven.contrib.django.raven_compat'
     ]
 
+    MIDDLEWARE = Common.MIDDLEWARE + [
+        # Placed at the bottom of the middleware stack to catch *any* 404s
+        'raven.contrib.django.raven_compat.middleware.Sentry404CatchMiddleware'
+    ]
+
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
     RAVEN_CONFIG = {
