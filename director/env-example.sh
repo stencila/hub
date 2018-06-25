45185585bd600f956dc59acc133490e26a62077f
Nokome Bentley <nokome@stenci.la>
2018-06-25 15:18:42 +1200

    Add bucket env vars; ignore env.sh in Docker build

--- director/env-example.sh
+++ director/env-example.sh
@@ -6,4 +6,7 @@
 echo 'Warning: using example development environment variables. See `env-examples.sh`'
 export DJANGO_SECRET_KEY='not-a-secret'
 export DJANGO_JWT_SECRET='not-a-secret'
+export DJANGO_GS_BUCKET_NAME='a-bucket-name'
+export DJANGO_GS_PROJECT_ID='a-project-id'
+
 export GOOGLE_APPLICATION_CREDENTIALS='path/to/credentials-file.json'
