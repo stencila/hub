3ad815e875bfdfe44db633aebba2d4ebf1b8d15d
Nokome Bentley <nokome@stenci.la>
2018-06-25 15:07:02 +1200

    Add env to examples

--- director/env-example.sh
+++ director/env-example.sh
@@ -6,3 +6,4 @@
 echo 'Warning: using example development environment variables. See `env-examples.sh`'
 export DJANGO_SECRET_KEY='not-a-secret'
 export DJANGO_JWT_SECRET='not-a-secret'
+export GOOGLE_APPLICATION_CREDENTIALS='path/to/credentials-file.json'
