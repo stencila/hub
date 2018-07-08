d77a3190df7f8e9bd75ffc3c286a25fc2af8cd45
Nokome Bentley <nokome@stenci.la>
2018-07-08 13:52:01 +1200

    Strip back requirements; use gunicorn instead of bjoern

--- director/director/wsgi.py
+++ director/director/wsgi.py
@@ -1,4 +1,16 @@
-# This is used by `python3 managepy runserver`
+"""
+WSGI config for director project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "director.settings")
+
 application = get_wsgi_application()
--- director/manage.py
+++ director/manage.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-import configurations
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "director.config.dev")
-    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "director.settings")
     try:
-        from configurations.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
