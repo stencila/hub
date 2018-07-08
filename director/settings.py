e774a1fe52028d5868f7ce8fd1cf397a9c4aff4c
Nokome Bentley <nokome@stenci.la>
2018-07-08 18:38:30 +1200

    Add a temporary home

--- director/settings.py
+++ director/settings.py
@@ -48,7 +48,9 @@ class Common(Configuration):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [],
+            'DIRS': [
+                os.path.join(BASE_DIR, 'templates')
+            ],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
--- director/urls.py
+++ director/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home')
 ]
