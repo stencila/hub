c22db8c92303e40f8c113f33a293d9b2710a7221
Nokome Bentley <nokome@stenci.la>
2018-07-16 13:04:01 +1200

    Add editors admin

--- director/editors/admin.py
+++ director/editors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
-# Register your models here.
+from .models import Editor, NativeEditor
+
+
+@admin.register(Editor)
+class EditorAdmin(ModelAdmin):
+    pass
+
+
+@admin.register(NativeEditor)
+class NativeEditorAdmin(ModelAdmin):
+    pass
