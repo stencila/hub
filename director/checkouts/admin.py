b6897a80dd888a96c860aef832f3fb7c0c297bc5
Nokome Bentley <nokome@stenci.la>
2018-07-23 16:02:41 -0400

    Add checkout events to admin

--- director/checkouts/admin.py
+++ director/checkouts/admin.py
@@ -1,15 +1,31 @@
 from django.contrib import admin
-from django.contrib.admin import ModelAdmin
 
-from .models import Checkout
+from .models import Checkout, CheckoutEvent
+
+
+class CheckoutEventInline(admin.TabularInline):
+    model = CheckoutEvent
+    readonly_fields = [
+        'time', 'type', 'topic', 'message', 'data'
+    ]
 
 
 @admin.register(Checkout)
-class CheckoutAdmin(ModelAdmin):
+class CheckoutAdmin(admin.ModelAdmin):
     list_display = [
         'id', 'project', 'creator', 'status', 'created', 'saved', 'closed'
     ]
-    list_select_related = (
+    list_select_related = [
         'creator',
         'project'
-    )
+    ]
+    inlines = [
+        CheckoutEventInline
+    ]
+
+
+@admin.register(CheckoutEvent)
+class CheckoutEventAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'checkout_id', 'time', 'type', 'topic'
+    ]
