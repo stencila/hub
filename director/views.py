a14256e715d51728ad4c2bde7ec52f13def6b2a6
Nokome Bentley <nokome@stenci.la>
2018-07-24 11:37:06 -0400

    Fix home view so it returns 200 for unauthenticated health check

--- director/views.py
+++ director/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import View
+
+from accounts.views import BetaTokenView
 
 
-class HomeView(View):
+class HomeView(BetaTokenView):
+    """
+    Home page view.
+
+    Care needs to be taken that this view returns a 200 response (not a redirect)
+    for unauthenticated users. This is because GCP load balancers ping the / path
+    as a health check and will fail if anything other than a 200 is returned.
+    """
 
     def get(self, *args, **kwargs):
         if self.request.user.is_authenticated:
             return redirect(reverse('project_list'))
         else:
-            return redirect(reverse('beta_token'))
+            return super().get(*args, **kwargs)
