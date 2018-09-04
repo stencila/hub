669d22a6b00f2fe205e86089e68849558f231f62
Ben Shaw <ben@beneboy.co.nz>
2018-09-04 16:50:19 +1200

    Added Team detail form for team CRU (no D yet)

--- director/accounts/views.py
+++ director/accounts/views.py
@@ -1,3 +1,5 @@
+import typing
+
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -6,9 +8,10 @@ from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.views.generic import View, ListView, DetailView, TemplateView, UpdateView
+from django.views.generic import View, ListView, DetailView, UpdateView
 
-from accounts.models import Account, AccountUserRole, AccountRole, AccountPermissionType
+from accounts.forms import TeamForm
+from accounts.models import Account, AccountUserRole, AccountRole, AccountPermissionType, Team
 
 User = get_user_model()
 
@@ -30,20 +33,41 @@ class AccountProfileView(LoginRequiredMixin, DetailView):
     template_name = 'accounts/account_profile.html'
 
 
-class AccountAccessView(LoginRequiredMixin, View):
-    @staticmethod
-    def fetch_account(request: HttpRequest, pk: int) -> Account:
-        account = get_object_or_404(Account, pk=pk)
+def fetch_account(user: User, account_pk: int) -> Account:
+    """
+    Fetches an account, raising exceptions: 404 if account does not exist, or PermissionDenied if user does not have
+    administrative permissions for the Account.
+    """
+    account = get_object_or_404(Account, pk=account_pk)
 
-        admin_roles = AccountRole.roles_with_permission(AccountPermissionType.ADMINISTER)
+    admin_roles = AccountRole.roles_with_permission(AccountPermissionType.ADMINISTER)
 
-        if AccountUserRole.objects.filter(user=request.user, account=account, role__in=admin_roles).count() == 0:
+    if AccountUserRole.objects.filter(user=user, account=account, role__in=admin_roles).count() == 0:
+        raise PermissionDenied
+
+    return account
+
+
+def fetch_team_for_account(account: Account, team_pk: typing.Optional[int]) -> Team:
+    """
+    Get Team with pk=team_pk. If team_pk is None a new Team is instantiated and returned (not saved to DB). The
+    function checks if the Team belongs to the passed in Account and if not raises PermissionDenied. It is assumed
+    that the current user already has administrative permissions for the Account and this has been checked.
+    """
+    if team_pk is not None:
+        team = get_object_or_404(Team, pk=team_pk)
+        if team.account != account:
             raise PermissionDenied
+    else:
+        team = Team()
+        team.account = account
 
-        return account
+    return team
 
+
+class AccountAccessView(LoginRequiredMixin, View):
     def get(self, request: HttpRequest, pk: int) -> HttpResponse:
-        account = self.fetch_account(request, pk)
+        account = fetch_account(request.user, pk)
 
         access_roles = AccountUserRole.objects.filter(account=account)
         all_roles = AccountRole.objects.all()
@@ -56,7 +80,7 @@ class AccountAccessView(LoginRequiredMixin, View):
         })
 
     def post(self, request: HttpRequest, pk: int) -> HttpResponse:
-        account = self.fetch_account(request, pk)
+        account = fetch_account(request.user, pk)
 
         all_roles = AccountRole.objects.all()
 
@@ -117,3 +141,39 @@ class AccountSettingsView(LoginRequiredMixin, UpdateView):
     model = Account
     fields = ['name']
     template_name = 'accounts/account_settings.html'
+
+
+class TeamDetailView(LoginRequiredMixin, View):
+    def get(self, request: HttpRequest, account_pk: int, team_pk: typing.Optional[int] = None) -> HttpResponse:
+        account = fetch_account(request.user, account_pk)  # if account is retrieved then user must have admin access
+        team = fetch_team_for_account(account, team_pk)
+
+        form = TeamForm(instance=team)
+
+        return render(request, "accounts/team_detail.html", {
+            "form": form
+        })
+
+    def post(self, request: HttpRequest, account_pk: int, team_pk: typing.Optional[int] = None) -> HttpResponse:
+        account = fetch_account(request.user, account_pk)  # if account is retrieved then user must have admin access
+        team = fetch_team_for_account(account, team_pk)
+
+        form = TeamForm(request.POST, instance=team)
+
+        if form.is_valid():
+            form.save()
+            if team_pk is None:
+                update_verb = "created"
+            else:
+                update_verb = "updated"
+
+            messages.success(request, "Team '{}' was {} successfully".format(team.name, update_verb))
+            return redirect(reverse('team_list', args=(account.id,)))
+
+        return render(request, "accounts/team_detail.html", {
+            "form": form
+        })
+
+
+class TeamListView(LoginRequiredMixin, View):
+    pass
