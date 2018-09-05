285551c96b94be4e3d2213d3dfbb9d49dbeb1223
Aleksandra Pawlik <aleksandra@stenci.la>
2018-09-05 14:06:20 +1200

    Don't really know what I am doing here but it may work once

--- director/scripts/create_dev_project_roles.py
+++ director/scripts/create_dev_project_roles.py
@@ -4,13 +4,17 @@ Assign roles to the test users (if any exist in the db).
 
 import random
 from django.contrib.auth.models import User
-from projects.models import Project
+from projects.models import Project, UserProjectRole, ProjectPermission
+from django.contrib.contenttypes.models import ContentType
 from django.conf import settings
 
 
 def random_role():
     roles = ['viewer', 'reader', 'reviewer', 'editor', 'manager', 'owner' ]
-    return random.choice(roles)
+
+    r = random.choice(roles)
+    if ProjectPermission.type == r:
+      return ProjectPermission
 
 def run(*args):
     # Ensure that this is only used in development
@@ -18,10 +22,17 @@ def run(*args):
 
     users = User.objects.all()
     projects = Project.objects.all()
+    user_type = ContentType.objects.get(app_label='auth', model='user')
+
+    ProjectPermission.objects.create()
 
     for user in users:
         for project in projects:
-           UserProjectRole.user = user
-           UserProjectRole.project = project
-           UserProjectRole.role = random_role()
-           UserProjectRole.save()
+          UserProjectRole.objects.create(
+           user = user,
+           agent_id = user.pk,
+           content_type = user_type,
+           project = project,
+           role = random_role(),
+           role_id = 1
+           )
