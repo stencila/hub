45f906bd35ce34a1a5fd1990a21df7160dbaafc1
Nokome Bentley <nokome@stenci.la>
2018-09-06 11:27:52 +1200

    Add AccountSettingsForm

--- director/accounts/forms.py
+++ director/accounts/forms.py
@@ -1,7 +1,23 @@
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 from django import forms
 
-from accounts.models import Team
+from accounts.models import Account, Team
+
+
+class AccountSettingsForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.layout = Layout(
+    	'name',
+        Submit('submit', 'Update', css_class="button is-primary")
+    )
+
+    class Meta:
+        model = Account
+        fields = ('name',)
+        widgets = {
+            'name': forms.TextInput
+        }
 
 
 class TeamForm(forms.ModelForm):
