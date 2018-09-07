06de3649a3b0342d3e3c061388bd39ca66f07e9a
Aleksandra Pawlik <aleksandra@stenci.la>
2018-09-07 16:34:23 +1200

    Fix to a small bug thanks to Nokome

--- director/accounts/views.py
+++ director/accounts/views.py
@@ -8,7 +8,7 @@ from django.db.models import QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import View, ListView, DetailView, UpdateView, CreateView
+from django.views.generic import View, ListView, DetailView, UpdateView
 
 from accounts.db_facade import AccountFetchResult, fetch_account
 from accounts.forms import AccountSettingsForm
@@ -118,6 +118,8 @@ class AccountAccessView(AccountPermissionsMixin, View):
 
         all_roles = AccountRole.objects.all()
 
+        account = self.account
+        
         role_lookup = {role.pk: role for role in all_roles}
 
         if request.POST.get('action') == 'add_access':
@@ -176,24 +178,6 @@ class AccountSettingsView(LoginRequiredMixin, UpdateView):
     model = Account
     form_class = AccountSettingsForm
     template_name = 'accounts/account_settings.html'
-
-    def get_success_url(self) -> str:
-        return reverse("account_profile", kwargs={'pk': self.object.pk})
-
-
-class AccountCreateView(LoginRequiredMixin, CreateView):
-    form_class = AccountCreateForm
-    template_name = "accounts/accounts_create.html"
-
-    def form_valid(self, form):
-        """
-        If the account creation form is valid them make the current user the
-        account creator (and owner)
-        """
-        self.object = form.save(commit=False)
-        #self.object.creator = self.request.user
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
 
     def get_success_url(self) -> str:
         return reverse("account_profile", kwargs={'pk': self.object.pk})
