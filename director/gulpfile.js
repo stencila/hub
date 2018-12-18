30e4ef4e18ccb5d656b9895047d917802a822627
Simon Winter <simon@saltedherring.com>
2018-12-19 09:21:18 +1300

    feature: added initial sass setup

---
+++ director/gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const moduleImporter = require('sass-module-importer')
+
+const sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded',
+    sourceMap: true,
+    importer: moduleImporter()
+}
+
+sass.compiler = require('node-sass')
+
+const sassDev = function() {
+  return gulp.src([
+    './node_modules/@stencila/style/sass/**/*.sass',
+    './assets/sass/**/*.sass'
+  ])
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./assets/css'))
+}
+
+const sassBuild = function() {
+  let options = sassOptions
+  options['outputStyle'] = 'collapsed'
+  
+  return gulp.src([
+    './node_modules/@stencila/style/sass/**/*.sass',
+    './assets/sass/**/*.sass'
+  ])
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./assets/css'))
+}
+
+const watch = function(done) {
+    gulp.watch('./node_modules/@stencila/style/sass/**/*.sass', { ignoreInitial: false }, gulp.series(sassDev))
+    gulp.watch('./assets/sass/**/*.sass', { ignoreInitial: false }, gulp.series(sassDev))
+    done()
+};
+
+exports.build = sassBuild
+exports.default = watch
+exports.watch = watch
