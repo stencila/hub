cb21dff52ac3ba222e432ba2d84974d15d5209cf
Aleksandra Pawlik <aleksandra@stenci.la>
2019-03-13 19:33:38 +1300

    feature: added pop up window

--- director/assets/js/ie-detect.js
+++ director/assets/js/ie-detect.js
@@ -1,17 +1,11 @@
 var sBrowser, sUsrAg = navigator.userAgent;
 
-Vue.component('modal', {
-    template: '#modal-template'
-  })
+
 
 if (sUsrAg.indexOf("MSIE") > -1) {
   sBrowser = "Microsoft Internet Explorer";
-  new Vue({
-    el: '#app',
-   showModal: true
-    }
-  ) 
+  
 
-//  document.write("You are using " + sBrowser + ". Unfortunately, Stencila Hub does not support this browser. Please use Chrome, Safari or Firefox."); 
+  alert("You are using " + sBrowser + ". Unfortunately, Stencila Hub does not support this browser. Please use Chrome, Safari or Firefox."); 
 }
 
