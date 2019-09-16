c553a285876fca529b2c09ce45745e27bb72be07
Ben Shaw <ben@beneboy.co.nz>
2019-09-17 08:39:29 +1200

    chore: Added more sleep time when waiting for DB

--- director/conversion-cleanup.sh
+++ director/conversion-cleanup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 until
-    python3 -c "import socket; socket.create_connection(('127.0.0.1', 5432))" > /dev/null  2>&1;
+    python3 -c "import socket; s = socket.create_connection(('127.0.0.1', 5432)); s.close()" > /dev/null  2>&1;
     do echo waiting for postgres;
     sleep 2;
 done;
+
+sleep 5
 
 python3 ./manage.py conversioncleanup
