54cddb4d5ae6aaa266e04ad6f96bc6cb2c0cb9eb
Ben Shaw <ben@beneboy.co.nz>
2019-11-22 11:40:41 +1300

    feat: Added JWT verify URL

--- director/api_urls.py
+++ director/api_urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from users.api_v1_urls import urlpatterns as user_v1_patterns
 from projects.api_v1_urls import urlpatterns as project_v1_patterns
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('v1/', include([
         path('grant-token/', obtain_jwt_token),
         path('refresh-token/', refresh_jwt_token),
+        path('verify-token/', verify_jwt_token),
         path('users/', include(user_v1_patterns)),
         path('projects/', include(project_v1_patterns))
     ]))
