102f1e4beba6924509b8ef042a0761e62eb7482c
Ben Shaw <ben@beneboy.co.nz>
2020-02-15 15:23:51 +1300

    fix: Fix/refactor GoogleAuthHelper and GoogleDocsFacade to be more sure of re-using updated credentials.

--- director/lib/google_docs_facade.py
+++ director/lib/google_docs_facade.py
@@ -49,7 +49,7 @@ class GoogleAuthHelper(object):
     client_id: str
     client_secret: str
     social_auth_token: typing.Optional[SocialToken]
-    _credentials: GoogleCredentials = None
+    _credentials: typing.Optional[GoogleCredentials] = None
 
     def __init__(self, client_id: str, client_secret: str,
                  social_auth_token: typing.Optional[SocialToken] = None) -> None:
@@ -63,53 +63,55 @@ class GoogleAuthHelper(object):
             return False
         return self.social_auth_token.expires_at is not None and self.social_auth_token.expires_at < timezone.now()
 
+    def build_credentials(self) -> typing.Optional[GoogleCredentials]:
+        """Build a `GoogleCredentials` object from the `SocialToken`/client IDs we have."""
+        if self.social_auth_token is None:
+            return None
+
+        return GoogleCredentials(self.social_auth_token.token, self.client_id, self.client_secret,
+                                 self.social_auth_token.token_secret, self.social_auth_token.expires_at,
+                                 GOOGLE_TOKEN_URI, 'Stencila Hub Client')
+
     @property
     def credentials(self) -> typing.Optional[GoogleCredentials]:
         if self._credentials is None:
-            if self.social_auth_token is None:
-                return None
-            else:
-                self._credentials = GoogleCredentials(self.social_auth_token.token, self.client_id, self.client_secret,
-                                                      self.social_auth_token.token_secret,
-                                                      self.social_auth_token.expires_at,
-                                                      GOOGLE_TOKEN_URI, 'Stencila Hub Client')
+            self._credentials = self.build_credentials()
+        self.check_and_refresh_token(self._credentials)
         return self._credentials
 
-    def update_social_auth_token(self, credentials: typing.Optional[GoogleCredentials]) -> None:
-        if credentials is None or self.social_auth_token is None:
+    def update_social_auth_token(self, credentials: GoogleCredentials) -> None:
+        """
+        Store the new token and expiry date from a `GoogleCredentials` object.
+
+        This is done after
+        """
+        if self.social_auth_token is None:
             return
         self.social_auth_token.token = credentials.access_token
         self.social_auth_token.expires_at = timezone.make_aware(credentials.token_expiry, timezone.utc)
         self.social_auth_token.save()
 
-    def check_and_refresh_token(self) -> None:
-        if self.credentials is None or not self.auth_token_expired:
+    def check_and_refresh_token(self, credentials: typing.Optional[GoogleCredentials]) -> None:
+        """Refresh the credentials (a `GoogleCredentials` object) from Google, if expired (according to us)."""
+        if credentials is None or not self.auth_token_expired:
             return
-        http = self.credentials.authorize(httplib2.Http())
-        self.credentials.refresh(http)
-        self.update_social_auth_token(self.credentials)
 
-    def get_credentials(self) -> GoogleCredentials:
-        self.check_and_refresh_token()
-        return self.credentials
+        http = credentials.authorize(httplib2.Http())
+        credentials.refresh(http)
+        self.update_social_auth_token(credentials)
 
 
 class GoogleDocsFacade(object):
-    client_id: str
-    client_secret: str
-    social_auth_token: typing.Optional[SocialToken]
     _drive_service = None
+    _auth_helper: GoogleAuthHelper
 
     def __init__(self, client_id: str, client_secret: str,
                  social_auth_token: typing.Optional[SocialToken] = None) -> None:
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.social_auth_token = social_auth_token
+        self._auth_helper = GoogleAuthHelper(client_id, client_secret, social_auth_token)
 
     @property
     def credentials(self) -> typing.Optional[GoogleCredentials]:
-        helper = GoogleAuthHelper(self.client_id, self.client_secret, self.social_auth_token)
-        return helper.get_credentials()
+        return self._auth_helper.credentials
 
     @property
     def drive_service(self):
@@ -130,8 +132,6 @@ class GoogleDocsFacade(object):
 
     def create_document(self, name: str, content: bytes, source_mimetype: str) -> str:
         """Create a Google Doc from a file and return its ID."""
-        drive_service = build('drive', 'v3', credentials=self.credentials, cache_discovery=False)
-
         metadata = {
             'name': name,
             'mimeType': 'application/vnd.google-apps.document'
@@ -139,7 +139,7 @@ class GoogleDocsFacade(object):
 
         document = MediaInMemoryUpload(content, mimetype=source_mimetype, resumable=True)
 
-        resp = drive_service.files().create(body=metadata, media_body=document, fields='id').execute()
+        resp = self.drive_service.files().create(body=metadata, media_body=document, fields='id').execute()
 
         return resp['id']
 
