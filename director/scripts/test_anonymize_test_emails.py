e988b83ab3af3239643c9a905a67391917400440
Nokome Bentley <nokome@stenci.la>
2020-03-18 13:09:34 +1300

    test(*): Move to pytest
    Moving from Nose to Pytest. The latter has several advantages, is more popular and we also use it in other Stencila projects e.g. pyla.

---
+++ director/scripts/test_anonymize_test_emails.py
@@ -0,0 +1,5 @@
+from scripts.anonymize_test_emails import anonymize_email
+
+def test_anonymize_email():
+    """Test that an email address has the @ replaced and domain set to example.com"""
+    assert anonymize_email("user@host.com") == "user.host.com@example.com"
