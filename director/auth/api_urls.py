c12117d5ba16dfaf9efb78bd1bdddc8f3a9e4e8a
Nokome Bentley <nokome@stenci.la>
2020-03-21 10:40:12 +1300

    fix(API URLs): Allow optional trailing slash

--- director/auth/api_urls.py
+++ director/auth/api_urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 
 from auth.api_views import GrantView, VerifyView, RefreshView, RevokeView
 
 urlpatterns = [
-    path("grant", GrantView.as_view(), name="api_auth_grant"),
-    path("verify", VerifyView.as_view(), name="api_auth_verify"),
-    path("refresh", RefreshView.as_view(), name="api_auth_refresh"),
-    path("revoke", RevokeView.as_view(), name="api_auth_revoke"),
+    re_path(r"grant/?", GrantView.as_view(), name="api_auth_grant"),
+    re_path(r"verify/?", VerifyView.as_view(), name="api_auth_verify"),
+    re_path(r"refresh/?", RefreshView.as_view(), name="api_auth_refresh"),
+    re_path(r"revoke/?", RevokeView.as_view(), name="api_auth_revoke"),
 ]
--- director/projects/api_urls.py
+++ director/projects/api_urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from projects.views.source_api_views import DiskItemCreateView, DiskItemMoveView, DiskItemRemoveView, SourceLinkView, \
     ItemPublishView, PublishedItemDeleteView
@@ -6,7 +6,8 @@ from projects.api_project_views import ProjectDetailView, ManifestView, ProjectL
     ProjectEventListView, AdminProjectEventListView
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='api_project_list'),
+    re_path(r"/?", ProjectListView.as_view(), name='api_project_list'),
+
     path('<int:pk>', ProjectDetailView.as_view(), name='api_project_detail'),
     path('<int:pk>/item-create/', DiskItemCreateView.as_view(), name='api_project_item_create'),
     path('<int:pk>/item-move/', DiskItemMoveView.as_view(), name='api_project_item_move'),
