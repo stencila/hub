e9bb00c6912b9ed348f01f90b0d7f6c244ae0298
Ben Shaw <ben@beneboy.co.nz>
2020-03-26 08:23:30 +1300

    feat: Account Admin users now get OWN permission on projects in their Account

--- director/projects/permission_facade.py
+++ director/projects/permission_facade.py
@@ -4,8 +4,9 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.shortcuts import get_object_or_404
 
+from accounts.db_facade import user_is_account_admin
 from accounts.models import Team
-from projects.permission_models import ProjectRole, ProjectAgentRole, ProjectPermissionType
+from projects.permission_models import ProjectRole, ProjectAgentRole, ProjectPermissionType, ProjectPermission
 from projects.project_models import Project
 
 User = get_user_model()
@@ -52,19 +53,26 @@ def fetch_project_for_user(user: AbstractUser, project_pk: typing.Optional[typin
 
     if not project:
         raise ValueError('Either pk or account_name and project_name or project must be provided.')
-    user_teams = Team.objects.filter(members=user) if user.is_authenticated else []
-
-    project_agent_roles = ProjectAgentRole.filter_with_user_teams(user, user_teams, project=project)
 
     roles: typing.Set[ProjectRole] = set()
     permissions: typing.Set[ProjectPermissionType] = set()
 
-    if project_agent_roles.count():
-        for project_agent_role in project_agent_roles:
-            add_roles_to_permissions_sets(roles, permissions, project_agent_role.role)
-    elif project.public:
-        read_only_role = ProjectRole.objects.get(name=READ_ONLY_PROJECT_ROLE_NAME)
+    if user.is_authenticated and user_is_account_admin(user, project.account):
+        # If user is an admin of the account, they have ProjectPermissionType.OWN  (highest) permission
+        own_permission = ProjectPermission.objects.get(type=ProjectPermissionType.OWN.value)
+        for role in ProjectRole.objects.filter(permissions__in=[own_permission]):
+            add_roles_to_permissions_sets(roles, permissions, role)
+    else:
+        user_teams = Team.objects.filter(members=user) if user.is_authenticated else []
 
-        add_roles_to_permissions_sets(roles, permissions, read_only_role)
+        project_agent_roles = ProjectAgentRole.filter_with_user_teams(user, user_teams, project=project)
+
+        if project_agent_roles.count():
+            for project_agent_role in project_agent_roles:
+                add_roles_to_permissions_sets(roles, permissions, project_agent_role.role)
+        elif project.public:
+            read_only_role = ProjectRole.objects.get(name=READ_ONLY_PROJECT_ROLE_NAME)
+
+            add_roles_to_permissions_sets(roles, permissions, read_only_role)
 
     return ProjectFetchResult(project, roles, permissions)
