a368bbb8ee6e643aa124ecfe590be88e59c1768e
Nokome Bentley <nokome@stenci.la>
2020-04-06 09:17:40 +1200

    chore(Tests): Formatting

--- director/users/ui/views_tests.py
+++ director/users/ui/views_tests.py
@@ -14,22 +14,31 @@ class SignInTestCase(DatabaseTestCase):
         """
         # without next parameter, we should not have the hidden next field in the HTML
         client = Client()
-        resp = client.get('/me/signin/')
+        resp = client.get("/me/signin/")
         self.assertNotIn(b'input type="hidden" name="next"', resp.content)
 
         # when posting, we get redirected to /
         login_resp = client.post("/me/signin/", {"login": "ada", "password": "ada"})
 
         # this is a bit fiddly since / redirects to /me/dashboard/
-        self.assertRedirects(login_resp, "/", fetch_redirect_response=False, target_status_code=302)
+        self.assertRedirects(
+            login_resp, "/", fetch_redirect_response=False, target_status_code=302
+        )
 
         # with the `next` parameter, it should be rendered in the form
         client = Client()
-        resp = client.get('/me/signin/?next=/test-redirect/')
-        self.assertIn(b'input type="hidden" name="next" value="/test-redirect/', resp.content)
+        resp = client.get("/me/signin/?next=/test-redirect/")
+        self.assertIn(
+            b'input type="hidden" name="next" value="/test-redirect/', resp.content
+        )
 
         # when posting, provide the `next` value that should be in the form
-        login_resp = client.post("/me/signin/", {"login": "ada", "password": "ada", "next": "/test-redirect/"})
+        login_resp = client.post(
+            "/me/signin/",
+            {"login": "ada", "password": "ada", "next": "/test-redirect/"},
+        )
 
         # we'll get a 404 since this page doesn't exists but it's OK we just want to check the location
-        self.assertRedirects(login_resp, "/test-redirect/", fetch_redirect_response=False)
+        self.assertRedirects(
+            login_resp, "/test-redirect/", fetch_redirect_response=False
+        )
