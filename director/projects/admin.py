1030ad4d66db5087a7ade3efa06efd6c43e46d1c
Nokome Bentley <nokome@stenci.la>
2020-04-07 12:27:55 +1200

    feat(Nodes): Add admin view

--- director/projects/admin.py
+++ director/projects/admin.py
@@ -1,4 +1,4 @@
-# flake8: noqa F401
+from django.contrib import admin
 
 from .project_admin import (
     ProjectAdmin,
@@ -8,3 +8,20 @@ from .project_admin import (
 )
 from .session_admin import SessionAdmin, SessionParametersAdmin, SessionRequestAdmin
 from .source_admin import SourceAdmin
+
+from .models import Node
+
+
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ["key", "creator", "created", "project", "app", "host"]
+    list_filter = ["app"]
+
+    # Optimizations to reduce number of queries made.
+    list_select_related = ["creator", "project"]
+    show_full_result_count = False
+
+    def get_queryset(self, request):
+        """Custom queryset to not fetch the `json` field (which can be large)."""
+        queryset = super().get_queryset(request)
+        return queryset.defer("json")
