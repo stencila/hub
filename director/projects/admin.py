5e381ba4dd7c896ac14611c3f842eca1a0e35755
Nokome Bentley <nokome@stenci.la>
2020-04-07 12:30:55 +1200

    chore(Nodes): Linting

--- director/projects/admin.py
+++ director/projects/admin.py
@@ -1,3 +1,4 @@
+# flake8: noqa F401
 from django.contrib import admin
 
 from .project_admin import (
@@ -22,6 +23,6 @@ class NodeAdmin(admin.ModelAdmin):
     show_full_result_count = False
 
     def get_queryset(self, request):
-        """Custom queryset to not fetch the `json` field (which can be large)."""
+        """Get custom queryset that does not fetch the `json` field (which can be large)."""
         queryset = super().get_queryset(request)
         return queryset.defer("json")
