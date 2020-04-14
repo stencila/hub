b0bd2849f8bf2658b5b8d1d5a66eea6e66d05fdd
Nokome Bentley <nokome@stenci.la>
2020-04-14 19:14:55 +1200

    chore(Scripts): Formatting

--- director/scripts/create_dev_project_sources.py
+++ director/scripts/create_dev_project_sources.py
@@ -11,15 +11,17 @@ def run(*args):
     projects by searching for a particular source.
     """
     for project in Project.objects.all():
-        FileSource.objects.create(
-            project=project, path="file-source"
-        )
+        FileSource.objects.create(project=project, path="file-source")
         GithubSource.objects.create(
             project=project, path="github-source", repo="org/{}".format(project.name)
         )
         GoogleDocsSource.objects.create(
-            project=project, path="google-docs-source", doc_id="gdoc-{}".format(project.name)
+            project=project,
+            path="google-docs-source",
+            doc_id="gdoc-{}".format(project.name),
         )
         UrlSource.objects.create(
-            project=project, path="url-source", url="http://example.org/{}".format(project.name)
+            project=project,
+            path="url-source",
+            url="http://example.org/{}".format(project.name),
         )
