959d01d2ce0dd635850e3b0820cf359cfcfae428
Nokome Bentley <nokome@stenci.la>
2020-04-17 15:46:38 +1200

    fix(Scheduler): Specify log level INFO

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -8,12 +8,16 @@ The definition of the periodic tasks is done in within the `director`
 database using django_celery_beat.
 """
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 import django
+
 django.setup()
 
 from celery import Celery
 from django_celery_beat.schedulers import DatabaseScheduler
 
-app = Celery(broker=os.environ.get("BROKER_URL"), scheduler=DatabaseScheduler)
+app = Celery(
+    broker=os.environ.get("BROKER_URL"), scheduler=DatabaseScheduler, loglevel="INFO"
+)
