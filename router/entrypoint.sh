70999db373986cf9dcf7f3d3108323d310973fb4
Nokome Bentley <nokome@stenci.la>
2020-04-17 17:12:50 +1200

    feat(Router): Allow passing of DIRECTOR_URL as env var

---
+++ router/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Docker ENTRYPOINT script to use `envsubst` to insert environment variables
+# into the Nginx config.
+# Thanks to https://serverfault.com/a/919212
+
+set -eu
+
+: "${DIRECTOR_URL:?Env var DIRECTOR_URL must be set and non-empty}"
+envsubst '${DIRECTOR_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+exec "$@"
