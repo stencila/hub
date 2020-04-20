ddd0abd6a35bdc9a550c2bd168a9a743627d52e9
Nokome Bentley <nokome@stenci.la>
2020-04-20 21:06:45 +1200

    fix(Scheduler): Point to dev db

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -4,8 +4,8 @@ Stencila Hub Scheduler.
 A Celery app for running `celery beat` to queue periodic tasks.
 See https://docs.celeryproject.org/en/latest/userguide/periodic-tasks.html.
 
-The definition of the periodic tasks is done in within the `director`
-database using django_celery_beat.
+The definition of the periodic tasks is done in within the
+database using `django_celery_beat`.
 """
 import os
 
--- scheduler/settings.py
+++ scheduler/settings.py
@@ -18,7 +18,7 @@ DATABASES = {"default": dj_database_url.config()}
 # Time zone awareness needs to be the same as in the database
 DJANGO_CELERY_BEAT_TZ_AWARE = False
 
-# Although not used and therefore not strictly necessary,
-# Django complains if this is empty. Use a random
+# Although not used, and therefore not strictly necessary,
+# Django complains if this is empty. We use a random
 # token just in case it is used.
 SECRET_KEY = secrets.token_hex(32)
