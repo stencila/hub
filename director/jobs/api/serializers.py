fa1e9f1969d428675ff64564c06b7c19d9eece93
Nokome Bentley <nokome@stenci.la>
2020-04-21 20:25:18 +1200

    feat(Job sessions): Allow for connection to a job session

--- director/jobs/api/serializers.py
+++ director/jobs/api/serializers.py
@@ -1,3 +1,4 @@
+from django.shortcuts import reverse
 from rest_framework import serializers
 
 from jobs.models import Job, JobMethod
@@ -15,6 +16,22 @@ class JobListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = "__all__"
+
+    url_global = serializers.SerializerMethodField()
+
+    def get_url_global(self, job: Job):
+        """
+        Get the URL that a user can connect to the job
+        from outside the local network.
+
+        Will be `None` if the job does not have an
+        internal URL or has ended.
+        """
+        if job.url and not job.ended:
+            request = self.context.get("request")
+            return request.build_absolute_uri(
+                reverse("api-jobs-connect", kwargs={"pk": job.id})
+            )
 
 
 class JobRetrieveSerializer(JobListSerializer):
@@ -46,8 +63,8 @@ class JobCreateSerializer(JobRetrieveSerializer):
     began = serializers.DateTimeField(read_only=True)
     ended = serializers.DateTimeField(read_only=True)
     result = serializers.JSONField(read_only=True)
-    log = serializers.JSONField(read_only=True)
     url = serializers.CharField(read_only=True)
+    log = serializers.JSONField(read_only=True)
     queue = serializers.CharField(read_only=True)
     worker = serializers.CharField(read_only=True)
     retries = serializers.IntegerField(read_only=True)
@@ -57,7 +74,7 @@ class JobUpdateSerializer(JobRetrieveSerializer):
     """
     A job serializer for the `update` and `partial_update` actions.
 
-    Makes some fields read only (should not be
+    Intended for `worker``. Makes some fields read only (should not be
     changed after creation) but allows workers to update
     values of rest.
     """
