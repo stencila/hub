a1331c4c8deb975d3ca2ed6ab1bff8bb3c11cca6
Richard Mansfield <richard@dragonfly.co.nz>
2020-05-13 10:21:08 +1200

    fix(Worker): Fetch figures in elife pull job

--- worker/jobs/pull/elife_test.py
+++ worker/jobs/pull/elife_test.py
@@ -4,8 +4,43 @@ from .elife import pull_elife
 
 
 def test_ok(tempdir):
+    figures = [
+        "fig1.jpg",
+        "fig1-figsupp1.jpg",
+        "fig1-figsupp2.jpg",
+        "fig1-figsupp3.jpg",
+        "fig1-figsupp4.jpg",
+        "fig1-figsupp5.jpg",
+        "fig2.jpg",
+        "fig3.jpg",
+        "fig4.jpg",
+        "fig4-figsupp1.jpg",
+        "fig4-figsupp2.jpg",
+        "fig5.jpg",
+        "fig5-figsupp1.jpg",
+        "fig6.jpg",
+        "fig6-figsupp1.jpg",
+        "fig6-figsupp2.jpg",
+        "fig7.jpg",
+        "fig7-figsupp1.jpg",
+        "fig8.jpg",
+        "fig8-figsupp1.jpg",
+        "resp-fig1.jpg",
+        "resp-fig2.jpg",
+        "resp-fig3.jpg",
+        "resp-fig4.jpg",
+        "resp-fig5.jpg",
+        "resp-fig6.jpg",
+        "resp-fig7.jpg",
+        "resp-fig8.jpg",
+        "resp-fig9.jpg",
+    ]
+
     pull_elife({"article": 45187}, tempdir.getpath("45187.jats.xml"))
-    tempdir.compare(["45187.jats.xml"])
+    tempdir.compare(
+        ["45187.jats.xml", "45187.jats.xml.media/"]
+        + ["45187.jats.xml.media/" + fig for fig in figures]
+    )
 
 
 def test_missing_article(tempdir):
