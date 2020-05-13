6a0cd669ccc3f6e107fd743d06adc60b7fd1f5e9
Richard Mansfield <richard@dragonfly.co.nz>
2020-05-13 16:11:28 +1200

    fix(Worker): Add malicious host test from director

--- worker/jobs/pull/http.py
+++ worker/jobs/pull/http.py
@@ -1,4 +1,11 @@
+import ipaddress
 import requests
+from socket import gethostbyname
+from urllib.parse import urlparse
+
+
+class RemoteFileException(Exception):
+    pass
 
 
 def pull_http(source: dict, sink: str) -> str:
@@ -7,12 +14,48 @@ def pull_http(source: dict, sink: str) -> str:
     """
     assert "url" in source, "HTTP source must have a URL"
 
-    session = requests.Session()
-    session.max_redirects = 5
-
-    with session.get(source["url"], stream=True, allow_redirects=True) as response:
+    with fetch_url(source["url"]) as response:
         with open(sink, "wb") as file:
             for chunk in response:
                 file.write(chunk)
 
     return sink
+
+
+def fetch_url(url):
+    url_obj = urlparse(url)
+
+    if url_obj.hostname is None:
+        raise TypeError('Url "{}" appears to have no hostname.'.format(url))
+
+    if is_malicious_host(url_obj.hostname):
+        raise RemoteFileException(
+            "{} is not a valid hostname.".format(url_obj.hostname)
+        )
+
+    session = requests.Session()
+    session.max_redirects = 5
+    headers = {"User-Agent": "Stencila Hub HTTP Client"}
+    return session.get(url, headers=headers, stream=True, allow_redirects=True)
+
+
+def is_malicious_host(hostname):
+    """Detect if user is trying to do things like connect to localhost
+    or a local IP address of some kind.
+    """
+    if not hostname:
+        return True
+
+    if hostname.lower() in ["hub-test.stenci.la", "hub.stenci.la"]:
+        return True
+
+    try:
+        ipaddress.ip_address(hostname)
+    except ValueError:
+        pass
+    else:
+        return True  # Trying to access a site by IP
+
+    address = gethostbyname(hostname)
+    ip = ipaddress.ip_address(address)
+    return ip.is_loopback or ip.is_private
---
+++ worker/jobs/pull/http_test.py
@@ -0,0 +1,15 @@
+import pytest
+
+from .http import pull_http, RemoteFileException
+
+
+def test_malicious_host():
+    with pytest.raises(RemoteFileException) as excinfo:
+        pull_http({"url": "https://10.1.2.3/x"}, "test.xml")
+    assert "10.1.2.3 is not a valid hostname" in str(excinfo.value)
+    with pytest.raises(RemoteFileException) as excinfo:
+        pull_http({"url": "https://hub.stenci.la/x"}, "test.xml")
+    assert "hub.stenci.la is not a valid hostname" in str(excinfo.value)
+    with pytest.raises(RemoteFileException) as excinfo:
+        pull_http({"url": "https://localhost/abc"}, "test.xml")
+    assert "localhost is not a valid hostname" in str(excinfo.value)
