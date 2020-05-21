a503941a89c87808fb1f85e1fe5ca88120e3e2ae
Richard Mansfield <richard@dragonfly.co.nz>
2020-05-21 13:25:54 +1200

    test(Worker): Recorded gdoc pull test

--- worker/jobs/pull/gdoc_test.py
+++ worker/jobs/pull/gdoc_test.py
@@ -1,13 +1,27 @@
+import os
 import pytest
 
 from .gdoc import pull_gdoc
 
+GOOGLE_TOKEN = (
+    "ya29.a0AfH6SMDJdyz151yBr4hQa8xNwCkUW5SNjK_LVQ6B21Rpius4sQOK6oDNEm7yYXG3_tf"
+    "fXM8dq1Z2k65zfZQ37FzYeVygEp93LB4_8gHEr1BDb9rZarmqjCoVUBoSOzyns1bHGYH_T15ka"
+    "O945eax1aJGZCR4vfYFNmyO"
+)
+
 
 def test_missing_token(tempdir):
+    doc_id = "1BW6MubIyDirCGW9Wq-tSqCma8pioxBI6VpeLyXn5mZA"
     with pytest.raises(AssertionError) as excinfo:
-        pull_gdoc(
-            dict(doc_id="1BW6MubIyDirCGW9Wq-tSqCma8pioxBI6VpeLyXn5mZA"),
-            tempdir.path,
-            "1BW6MubIyDirCGW9Wq-tSqCma8pioxBI6VpeLyXn5mZA.json",
-        )
+        pull_gdoc(dict(doc_id=doc_id), tempdir.path, "{}.json".format(doc_id))
     assert "source must include a token" in str(excinfo.value)
+
+
+@pytest.mark.vcr
+def test_ok(tempdir):
+    doc_id = "14z9ScjW4gVjPBRw5XfIdA5LxrApUJx3-S7cXgdNvElc"
+    doc_json = "{}.json".format(doc_id)
+    pull_gdoc(
+        dict(doc_id=doc_id, token=GOOGLE_TOKEN,), tempdir.path, doc_json,
+    )
+    assert os.path.exists(os.path.join(tempdir.path, doc_json))
