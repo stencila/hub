839d726f362bc054adbf96ba4f895563e476a0bb
Richard Mansfield <richard@dragonfly.co.nz>
2020-05-27 13:21:55 +1200

    fix(Director): Use verified github emails in match
    When a user logs in from github, check all their verified github emails
    against the database before deciding to create a new user.

--- director/lib/social_account_adapter.py
+++ director/lib/social_account_adapter.py
@@ -1,3 +1,4 @@
+import requests
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
@@ -18,6 +19,8 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
         if not sociallogin.email_addresses:
             return
 
+        self.add_secondary_emails(sociallogin)
+
         for email in sociallogin.email_addresses:
             if not email.verified:
                 continue
@@ -29,3 +32,45 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
                 break
             except EmailAddress.DoesNotExist:
                 continue
+
+    # Allauth's github provider currently only has a primary email, but
+    # we would like secondary emails too, so add an extra request to the
+    # gihub api for all user emails. It could be cleaner to override
+    # GitHubProvider's extract_email_addresses function, but here we can
+    # restrict the extra request to the first login instead of every
+    # login.
+    def add_secondary_emails(self, sociallogin):
+        if len(sociallogin.email_addresses) != 1:
+            return
+
+        if sociallogin.account.provider != "github":
+            return
+
+        response = requests.get(
+            "https://api.github.com/user/emails",
+            headers={"Authorization": "token {}".format(sociallogin.token.token)},
+        )
+
+        if response.status_code != 200:
+            # No access to user/emails, but assume the primary address
+            # is verified, because github requires email verification
+            # before allowing authorization of oauth apps.
+            # https://help.github.com/en/github/authenticating-to-github/authorizing-oauth-apps
+            sociallogin.email_addresses[0].verified = True
+            return
+
+        all_emails = response.json()
+
+        if all_emails:
+            email_addresses = []
+
+            for email in all_emails:
+                email_addresses.append(
+                    EmailAddress(
+                        email=email.get("email"),
+                        verified=email.get("verified"),
+                        primary=email.get("primary"),
+                    )
+                )
+
+            sociallogin.email_addresses = email_addresses
