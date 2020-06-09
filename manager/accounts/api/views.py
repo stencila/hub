43138d4885e2cf5ec1088c823052ba72630fd213
Nokome Bentley <nokome@stenci.la>
2020-06-09 15:04:58 +1200

    refactor(Manager): Move HtmxMixin so it can be shared

--- manager/accounts/api/views.py
+++ manager/accounts/api/views.py
@@ -1,10 +1,6 @@
-from typing import Type
-
 from django.db.models import Prefetch
 from django.shortcuts import reverse
-from djangorestframework_camel_case.render import CamelCaseJSONRenderer
 from rest_framework import exceptions, mixins, permissions, viewsets
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -21,51 +17,8 @@ from accounts.api.serializers import (
     TeamUpdateSerializer,
 )
 from accounts.models import Account, AccountQuotas, AccountRole, AccountUser, Team
-from manager.api.helpers import filter_from_ident
+from manager.api.helpers import HtmxMixin, filter_from_ident
 from users.api.serializers import UserIdentifierSerializer
-
-
-class HtmxMixin:
-
-    renderer_classes = [CamelCaseJSONRenderer, TemplateHTMLRenderer]
-
-    CREATED = 201
-    RETRIEVED = 200
-    UPDATED = 210
-    DESTROYED = 204
-    INVALID = 211
-
-    @classmethod
-    def init(
-        cls: Type[viewsets.ViewSet], action: str, request: Request, args, kwargs
-    ) -> viewsets.ViewSet:
-        """
-        Create a Django Rest Framework `ViewSet` instance for a request.
-
-        This replicates the `as_view` method of the `ViewSetMixin` class
-        so that we can re-use of the permissions and filtering logic defined
-        in the DRF view sets within templated views e.g.
-
-        viewset = AccountsViewSet.init("create", request, args, kwargs)
-        serializer.get_serializer()
-
-        """
-        vs = cls()
-        vs.action = action
-        vs.request = request
-        vs.args = args
-        vs.kwargs = kwargs
-        vs.format_kwarg = None
-        return vs
-
-    def is_html(self):
-        return self.request.META.get("HTTP_ACCEPT") == "text/html"
-
-    def get_template_names(self):
-        template = self.request.META.get("HTTP_X_HX_TEMPLATE")
-        if template:
-            return [template]
-        return ["api/_default.html"]
 
 
 class AccountsViewSet(
--- manager/manager/api/helpers.py
+++ manager/manager/api/helpers.py
@@ -1,9 +1,56 @@
 """Helper functions for API views."""
 
 import re
-from typing import Dict, Optional, Union
+from typing import Dict, Optional, Type, Union
 
 from django.shortcuts import get_object_or_404
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
+from rest_framework import viewsets
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.request import Request
+
+
+class HtmxMixin:
+
+    renderer_classes = [CamelCaseJSONRenderer, TemplateHTMLRenderer]
+
+    CREATED = 201
+    RETRIEVED = 200
+    UPDATED = 210
+    DESTROYED = 204
+    INVALID = 211
+
+    @classmethod
+    def init(
+        cls: Type[viewsets.ViewSet], action: str, request: Request, args, kwargs
+    ) -> viewsets.ViewSet:
+        """
+        Create a Django Rest Framework `ViewSet` instance for a request.
+
+        This replicates the `as_view` method of the `ViewSetMixin` class
+        so that we can re-use of the permissions and filtering logic defined
+        in the DRF view sets within templated views e.g.
+
+        viewset = AccountsViewSet.init("create", request, args, kwargs)
+        serializer.get_serializer()
+
+        """
+        vs = cls()
+        vs.action = action
+        vs.request = request
+        vs.args = args
+        vs.kwargs = kwargs
+        vs.format_kwarg = None
+        return vs
+
+    def is_html(self):
+        return self.request.META.get("HTTP_ACCEPT") == "text/html"
+
+    def get_template_names(self):
+        template = self.request.META.get("HTTP_X_HX_TEMPLATE")
+        if template:
+            return [template]
+        return ["api/_default.html"]
 
 
 def filter_from_ident(
