08b3bdd846f19f70ffb45e657507d3b2b443ad02
Nokome Bentley <nokome@stenci.la>
2020-06-10 14:01:11 +1200

    chore(Manager): Make development database richer

--- manager/scripts/create_dev_db.py
+++ manager/scripts/create_dev_db.py
@@ -1,4 +1,7 @@
+import random
+
 from django.conf import settings
+from django.db.utils import IntegrityError
 
 from accounts.models import Account, AccountRole, AccountTeam, AccountUser
 from projects.models import Project
@@ -26,7 +29,7 @@ def run(*args):
     admin.is_superuser = True
     admin.save()
 
-    # Staff member (not super user)
+    # Staff member (not super user, but can access admin interface)
     staff = User.objects.create_user(
         username="staff",
         password="staff",
@@ -36,17 +39,16 @@ def run(*args):
     staff.is_staff = True
     staff.save()
 
-    # Normal users
-    user = User.objects.create_user(
-        username="user", password="user", first_name="User", email="user@example.com",
-    )
-    user.save()
-
     for spec in [
-        ("joe", "Joe", "Blogs"),
-        ("jane", "Jane", "Doe"),
-        ("mike", "Mike", "Morris"),
-        ("mary", "Mary", "Jones"),
+        # Generic users that are added to EVERY account in these different roles
+        ("member", "Member", "User"),
+        ("manager", "Manager", "User"),
+        ("owner", "Owner", "User"),
+        # Example users
+        ("amitabh", "Amitabh", "Modi"),
+        ("anna", "Anna", "Andersson"),
+        ("daniel", "Daniel", "Colreavy"),
+        ("zahra", "Zahra", "Temitope"),
     ]:
         User.objects.create_user(
             username=spec[0],
@@ -56,28 +58,100 @@ def run(*args):
             email=spec[0] + "@example.com",
         )
 
+    member = User.objects.get(username="member")
+    manager = User.objects.get(username="manager")
+    owner = User.objects.get(username="owner")
+
+    # Add varying amounts of profile data for these example users
+
+    amitabh = User.objects.get(username="amitabh").personal_account
+    amitabh.location = "Sydney, Australia"
+    amitabh.image.save("amitabh.png", open("scripts/data/amitabh.png", "rb"))
+
+    anna = User.objects.get(username="anna").personal_account
+    anna.display_name = "Dr Anna Andersson"
+    anna.location = "Stockholm, Sweden"
+    anna.website = "https://anna.example.edu/profile.html"
+    anna.email = "anna@example.edu"
+    anna.image.save("anna.png", open("scripts/data/anna.png", "rb"))
+
+    daniel = User.objects.get(username="daniel").personal_account
+    daniel.location = "Dublin"
+    daniel.email = "daniel@example.edu"
+    daniel.image.save("daniel.png", open("scripts/data/daniel.png", "rb"))
+
+    zahra = User.objects.get(username="zahra").personal_account
+    zahra.location = "Nairobi, Kenya, East Africa"
+    zahra.website = "https://example.com"
+    zahra.image.save("zahra.png", open("scripts/data/zahra.png", "rb"))
+
     #################################################################
     # Accounts
     #################################################################
 
-    for name in ["stencila", "hapuku-university", "craigiburn-college", "acme-ltd"]:
+    # A stencila account is needed as the default for running jobs etc
+
+    stencila = Account.objects.create(name="stencila")
+
+    # Example organizations
+
+    for name in ["biotech-corp", "hapuku-university", "pewsey-publishing"]:
         account = Account.objects.create(name=name)
 
-        AccountUser.objects.create(
-            account=account, user=user, role=AccountRole.MEMBER.name
-        )
-        AccountUser.objects.create(
-            account=account, user=staff, role=AccountRole.MANAGER.name
-        )
-        AccountUser.objects.create(
-            account=account, user=admin, role=AccountRole.OWNER.name
+        # Add their image
+        account.image.save(
+            "account-logo.png", open("scripts/data/{0}.png".format(name), "rb")
         )
 
-        a_team = AccountTeam.objects.create(account=account, name="a-team")
-        a_team.members.set([user, staff, admin])
+        # The generic users have their roles on every account
+        AccountUser.objects.create(
+            account=account, user=member, role=AccountRole.MEMBER.name
+        )
+        AccountUser.objects.create(
+            account=account, user=manager, role=AccountRole.MANAGER.name
+        )
+        AccountUser.objects.create(
+            account=account, user=owner, role=AccountRole.OWNER.name
+        )
 
-        b_team = AccountTeam.objects.create(account=account, name="b-team")
-        b_team.members.set(random_users(3))
+        # In addition, some other randome users are added with random roles
+        for user in random_users():
+            try:
+                AccountUser.objects.create(
+                    account=account, user=user, role=random_account_role()
+                )
+            except IntegrityError:
+                pass
+
+        # Each account has a "first-team" team with the generic users, plus a random
+        # number of random users
+        team1 = AccountTeam.objects.create(account=account, name="first-team")
+        team1.members.set([member, owner, manager] + random_users())
+
+        # Each account has a random number of others teams each 
+        # with a random name and random number of random users 
+        # (who don't need to be be users of the organizations)
+        for index in range(random.randint(1, 5)):
+            team = AccountTeam.objects.create(account=account, name=random_team_name())
+            team.members.set(random_users())
+
+    biotech = Account.objects.get(name="biotech-corp")
+    biotech.display_name = "Biotech Corporation"
+    biotech.location = "Portland, USA"
+    biotech.website = "https://biotech.example.com"
+    biotech.email = "contact@biotech.example.com"
+
+    hapuku = Account.objects.get(name="hapuku-university")
+    hapuku.display_name = "Hapuku University"
+    hapuku.location = "Hapuku, Kaikoura, New Zealand"
+    hapuku.website = "https://hapuku.example.edu"
+    hapuku.email = "contact@hapuku.example.edu"
+
+    pewsey = Account.objects.get(name="pewsey-publishing")
+    pewsey.display_name = "Pewsey Publishing"
+    pewsey.location = "UK"
+    pewsey.website = "https://pewsey.example.com"
+    pewsey.email = "contact@pewsey.example.com"
 
     #################################################################
     # Projects
@@ -114,9 +188,24 @@ xcepteur sint occaecat cupidatat non roident, sunt in culpa qui officia deserunt
     )
 
 
-def random_users(num):
-    return User.objects.order_by("?").all()[:num]
+def random_users(num=None):
+    """Get a random set of users."""
+    all = User.objects.order_by("?").all()
+    if num is None:
+        num = random.randint(1, len(all))
+    return all[:num]
 
 
 def random_account_user(account):
+    """Get a random user for an account."""
     return AccountUser.objects.filter(account=account).order_by("?").first().user
+
+def random_account_role():
+    """Get a random account role."""
+    return random.choice([e.name for e in AccountRole])
+
+def random_team_name():
+    """Get a random team name."""
+    return random.choice(
+        ["editors", "authors", "reviewers", "special-project-team", "vision-2020-team"]
+    )
