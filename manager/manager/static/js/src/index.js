8a3170dca18a497529901ae244a5079b23f46689
alex-ketch <alex@ketch.me>
2020-06-12 18:02:06 -0400

    feat(Sources): Move New/Delete Source forms to a styled modal

--- manager/manager/static/js/src/index.js
+++ manager/manager/static/js/src/index.js
@@ -7,6 +7,43 @@ import { initDropdowns } from "./dropdowns";
 import "./htmx";
 import "./htmx-extensions.js";
 
+// =============================================================================
+// Modal container logic
+// =============================================================================
+const openModal = () => {
+  const modalTarget = document.querySelector("#modal-target");
+  if (modalTarget) {
+    modalTarget.classList.add("is-active");
+  }
+};
+
+const closeModal = () => {
+  const modalTarget = document.querySelector("#modal-target");
+  if (modalTarget) {
+    modalTarget.classList.remove("is-active");
+  }
+};
+
+const handleModalClose = () => {
+  const modalBackground = document.querySelector(".modal-background");
+  if (modalBackground) {
+    modalBackground.addEventListener("click", () => {
+      closeModal();
+      // Remove HTMX inserted content
+      let nextSibling;
+      while(nextSibling = modalBackground.nextElementSibling) {
+        nextSibling.remove();
+      }
+    });
+  }
+}
+
+// ==============================================================================
+
+/**
+ * Wait for document to be ready before calling the callback function.
+ * Ensures that page is ready, and all elements present before performing any logic.
+ */
 const onReady = (cb) => {
   // see if DOM is already available
   if (
@@ -20,11 +57,25 @@ const onReady = (cb) => {
   }
 };
 
-onReady(() => {
+const attachAllEventHandlers = () => {
   initDropdowns();
+  handleModalClose()
+};
 
-  // Ensure that dynamically added DOM elements have dropdown event handlers
-  htmx.on("afterSwap.htmx", () => {
-    initDropdowns();
-  });
+onReady(attachAllEventHandlers);
+
+// =============================================================================
+// HTMX Event handlers
+// =============================================================================
+htmx.on("afterSwap.htmx", (e) => {
+  // Activate the modal container when inserteing elements into it
+  if (e.detail.target.id.includes("modal-target")) {
+    openModal();
+  }
 });
+
+// Ensure that dynamically added DOM elements have dropdown event handlers
+htmx.on("load.htmx", () => {
+  attachAllEventHandlers()
+});
+
