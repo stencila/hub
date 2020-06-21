c94481435b95f818b3e16a49f0d5c0e58e80e0c7
Nokome Bentley <nokome@stenci.la>
2020-06-22 11:10:10 +1200

    refactor(Router): Swap out director for manager

--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -6,9 +6,9 @@
 
 set -eu
 
-: "${DIRECTOR_HOST:?Env var DIRECTOR_HOST must be set and non-empty}"
+: "${MANAGER_HOST:?Env var MANAGER_HOST must be set and non-empty}"
 : "${MONITOR_URL:?Env var MONITOR_URL must be set and non-empty}"
 
-envsubst '${DIRECTOR_HOST} ${MONITOR_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${MANAGER_HOST} ${MONITOR_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
