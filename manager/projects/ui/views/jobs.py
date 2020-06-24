80ded471911f7f874d15ac3eb045380854f56c9a
Nokome Bentley <nokome@stenci.la>
2020-06-25 00:21:55 +1200

    feat(Jobs): Add job list and show children of compound jobs

--- manager/projects/ui/views/jobs.py
+++ manager/projects/ui/views/jobs.py
@@ -3,7 +3,15 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
 from jobs.api.views import ProjectsJobsViewSet
-from projects.models.projects import Project
+
+
+@login_required
+def list(request: HttpRequest, *args, **kwargs) -> HttpResponse:
+    """Get a list of project jobs."""
+    viewset = ProjectsJobsViewSet.init("list", request, args, kwargs)
+    project = viewset.get_project()
+    jobs = viewset.get_queryset()
+    return render(request, "projects/jobs/list.html", dict(project=project, jobs=jobs))
 
 
 @login_required
@@ -11,28 +19,21 @@ def retrieve(request: HttpRequest, *args, **kwargs) -> HttpResponse:
     """
     Retrieve a job.
     """
-    # Resolve the account + project names into a project id
-    project = Project.objects.get(
-        account__name=kwargs.get("account"), name=kwargs.get("project")
-    )
-
-    # Get the job (with permissions checking)
-    viewset = ProjectsJobsViewSet.init(
-        "retrieve", request, args, dict(project=str(project.id), job=kwargs.get("job"))
-    )
+    viewset = ProjectsJobsViewSet.init("retrieve", request, args, kwargs)
+    project = viewset.get_project()
     job = viewset.get_object()
 
     # If the ?redirect parameter is present
     # then check if the job is already ready and we
     # can redirect to the result directly
     redirect_on_success = request.GET.get("redirect")
-    if redirect_on_success is not None:
-        if job.has_ended:
-            # TODO: Redirect to the result of the job
-            return redirect(job.success_url())
+    # if redirect_on_success is not None:
+    #    if job.has_ended:
+    #        # TODO: Redirect to the result of the job
+    #        return redirect(job.success_url())
 
     return render(
         request,
         "projects/jobs/retrieve.html",
-        dict(job=job, redirect_on_success=redirect_on_success),
+        dict(project=project, job=job, redirect_on_success=redirect_on_success),
     )
