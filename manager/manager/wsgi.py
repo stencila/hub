2dc115c8d81ab0adf4e16c6f962a8913e0a53702
Nokome Bentley <nokome@stenci.la>
2020-07-07 18:04:33 +1200

    fix(Manager): Fix Docker image build; add run-docker recipe

--- manager/manager/wsgi.py
+++ manager/manager/wsgi.py
@@ -3,6 +3,6 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manager.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
 
-from django.core.wsgi import get_wsgi_application  # noqa: E402
+from configurations.wsgi import get_wsgi_application  # noqa: E402
 
 application = get_wsgi_application()
