bda8930dfca8f4eef4ffd82758c33bf26ea99a13
Nokome Bentley <nokome@stenci.la>
2020-07-09 13:19:38 +1200

    fix(Manager): Remove stencibot from migration

--- manager/accounts/migrations/0002_models.py
+++ manager/accounts/migrations/0002_models.py
@@ -1,5 +1,4 @@
 from django.db import migrations
-from django.contrib.auth.hashers import make_password
 
 
 def initial_models(apps, *args):
@@ -14,16 +13,6 @@ def initial_models(apps, *args):
     # Need a Stencila account to own the default job queue etc
     Account = apps.get_model("accounts", "Account")
     stencila = Account.objects.create(name="stencila", display_name="Stencila")
-
-    # A staff user is needed for use by internal services
-    # e.g. for the `overseer` service to authenticate to post job and worker updates
-    User = apps.get_model("auth", "User")
-    stencibot = User.objects.create(
-        username="stencibot",
-        password=make_password("stencibot"),
-        is_staff=True,
-        is_active=True,
-    )
 
 
 class Migration(migrations.Migration):
