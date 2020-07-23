501e62f189fa052495b03c0c9a46092e6479e948
alex-ketch <alex@ketch.me>
2020-07-22 21:17:11 -0400

    feat(JS): Add User Flow js for creating product tours

--- manager/manager/static/js/src/libs.js
+++ manager/manager/static/js/src/libs.js
@@ -3,6 +3,7 @@
 export { File } from "@vizuaalog/bulmajs/src/plugins/file";
 export { Navbar } from "@vizuaalog/bulmajs/src/plugins/navbar";
 export { toast as Toast } from "bulma-toast";
+export userflow from "userflow.js";
 // `htmx` is available globally
 import "htmx.org";
 import "htmx.org/dist/ext/include-vals";
