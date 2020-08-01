d6c103b99ea847aa1edab922977ce31f1ee59a56
Nokome Bentley <nokome@stenci.la>
2020-08-01 18:18:44 +1200

    feat(Manage jobs): Add cancel button and minor template tidyups

--- manager/projects/ui/views/jobs.py
+++ manager/projects/ui/views/jobs.py
@@ -7,11 +7,14 @@ from jobs.api.views import ProjectsJobsViewSet
 
 @login_required
 def list(request: HttpRequest, *args, **kwargs) -> HttpResponse:
-    """Get a list of project jobs."""
+    """
+    Get a list of project jobs.
+    """
     viewset = ProjectsJobsViewSet.init("list", request, args, kwargs)
     project = viewset.get_project()
     jobs = viewset.get_queryset(project)
-    return render(request, "projects/jobs/list.html", dict(jobs=jobs, project=project))
+
+    return render(request, "projects/jobs/list.html", dict(project=project, jobs=jobs))
 
 
 @login_required
@@ -23,17 +26,6 @@ def retrieve(request: HttpRequest, *args, **kwargs) -> HttpResponse:
     project = viewset.get_project()
     job = viewset.get_object(project)
 
-    # If the ?redirect parameter is present
-    # then check if the job is already ready and we
-    # can redirect to the result directly
-    redirect_on_success = request.GET.get("redirect")
-    # if redirect_on_success is not None:
-    #    if job.has_ended:
-    #        # TODO: Redirect to the result of the job
-    #        return redirect(job.success_url())
-
     return render(
-        request,
-        "projects/jobs/retrieve.html",
-        dict(job=job, project=project, redirect_on_success=redirect_on_success),
+        request, "projects/jobs/retrieve.html", dict(project=project, job=job)
     )
