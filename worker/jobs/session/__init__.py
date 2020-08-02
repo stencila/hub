13b3ca3eb71db987ae4623cd71df60738eae17b5
Nokome Bentley <nokome@stenci.la>
2020-08-02 19:39:04 +1200

    feat(Session jobs): Switch session type based on environ

--- worker/jobs/session/__init__.py
+++ worker/jobs/session/__init__.py
@@ -1,10 +1,17 @@
+import os
 from typing import Optional, List
 
 from jobs.base.job import Job
 
-from .subprocess_session import SubprocessSession
-from .kubernetes_session import KubernetesSession
+# If on a K8s cluster then use the K8s-based sessions
+# otherwise use the subsprocess-based session
+if "KUBERNETES_SERVICE_HOST" in os.environ:
+    from .kubernetes_session import KubernetesSession
 
-# Session = KubernetesSession
-Session = SubprocessSession
+    Session = KubernetesSession
+else:
+    from .subprocess_session import SubprocessSession
+
+    Session = SubprocessSession
+
 Session.name = "session"
