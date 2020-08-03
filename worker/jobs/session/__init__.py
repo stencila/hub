3706700e4725d23752269c2e833adfa736d0ce96
Nokome Bentley <nokome@stenci.la>
2020-08-03 18:15:20 +1200

    fix(Worker): Improve switching between session types

--- worker/jobs/session/__init__.py
+++ worker/jobs/session/__init__.py
@@ -1,17 +1,14 @@
-import os
-from typing import Optional, List
+from typing import Type, Union
 
-from jobs.base.job import Job
+from .kubernetes_session import api_instance, KubernetesSession
+from .subprocess_session import SubprocessSession
 
-# If on a K8s cluster then use the K8s-based sessions
+# If on a K8s is available then use that
 # otherwise use the subsprocess-based session
-if "KUBERNETES_SERVICE_HOST" in os.environ:
-    from .kubernetes_session import KubernetesSession
-
-    Session = KubernetesSession  # type: ignore
+Session: Type[Union[KubernetesSession, SubprocessSession]]
+if api_instance is not None:
+    Session = KubernetesSession
 else:
-    from .subprocess_session import SubprocessSession
-
-    Session = SubprocessSession  # type: ignore
+    Session = SubprocessSession
 
 Session.name = "session"
