72ea49ef3d2e64db1c58952c75a572854023d969
Nokome Bentley <nokome@stenci.la>
2020-08-07 16:06:58 +1200

    fix(Router): Proxy external addresses using resolver

--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -8,7 +8,8 @@ set -eu
 
 : "${MANAGER_HOST:?Env var MANAGER_HOST must be set and non-empty}"
 : "${MONITOR_URL:?Env var MONITOR_URL must be set and non-empty}"
+: "${RESOLVER_ADDRESS:?Env var RESOLVER_ADDRESS must be set and non-empty}"
 
-envsubst '${MANAGER_HOST} ${MONITOR_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${MANAGER_HOST} ${MONITOR_URL} ${RESOLVER_ADDRESS}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
