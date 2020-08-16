3bcbd807c98d411f6027d5cbc55486e9e1df4522
Nokome Bentley <nokome@stenci.la>
2020-08-16 15:44:19 +1200

    fix(Router): Optimize Nginx config

--- router/entrypoint.sh
+++ router/entrypoint.sh
@@ -2,13 +2,6 @@
 
 set -eu
 
-# Increase the number of worker connections from the
-# default of 1024 to avoid the warning "1024 worker_connections are not enough, reusing connections"
-# observed during stress testing.
-# Needs to be set in /etc/nginx/nginx.conf, not our /etc/nginx/conf.d/default.conf
-# See https://craftypixels.com/worker-connections-are-not-enough/
-sed -i -e "s/worker_connections  1024/worker_connections  2048/g" /etc/nginx/nginx.conf
-
 # Use `envsubst` to insert environment variables into the Nginx config.
 # Thanks to https://serverfault.com/a/919212
 
@@ -16,6 +9,6 @@ sed -i -e "s/worker_connections  1024/worker_connections  2048/g" /etc/nginx/ngi
 : "${MONITOR_URL:?Env var MONITOR_URL must be set and non-empty}"
 : "${RESOLVER_ADDRESS:?Env var RESOLVER_ADDRESS must be set and non-empty}"
 
-envsubst '${MANAGER_HOST} ${MONITOR_URL} ${RESOLVER_ADDRESS}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${MANAGER_HOST} ${MONITOR_URL} ${RESOLVER_ADDRESS}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec "$@"
