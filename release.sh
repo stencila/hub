e52b58c337cfc5dfaaa2b6260ae2d3f41453b0c4
Nokome Bentley <nokome@stenci.la>
2020-10-06 08:59:03 +1300

    chore(Release): Use uppercase C to change dir

--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ sed -i -e "s!__version__ = .*!__version__ = \"$1\"!g" manager/manager/version.py
 
 # Update the version in the Python client and regenerate the files
 sed -i -e "s!packageVersion: .*!packageVersion: $1!g" clients/python/.openapi-generator-config.yaml
-make -c clients python python-publish
+make -C clients python python-publish
 
 # Update the version in the Typescript client and regenerate the files
 sed -i -e "s!\"version\": .*!\"version\": \"$1\",!g" clients/typescript/package.json
-make -c clients typescript typescript-publish
+make -C clients typescript typescript-publish
