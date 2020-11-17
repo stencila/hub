2da189e3d75bd91c9f1616dc97a90ce349931cc3
Nokome Bentley <nokome@stenci.la>
2020-11-17 15:55:55 +1300

    fix(Github pull): Fetch zip archive of repo.
    Closes #831. Avoids excessive use of API to avoid API rate limits.

--- worker/jobs/pull/github.py
+++ worker/jobs/pull/github.py
@@ -1,11 +1,11 @@
-import base64
 import os
-import shutil
-from io import BytesIO
+import tempfile
+from pathlib import Path
 from typing import List, Optional
+from zipfile import ZipFile
 
+import httpx
 from github import Github
-from github.ContentFile import ContentFile
 
 from util.files import Files, file_info
 
@@ -25,11 +25,6 @@ def pull_github(
 
     If a user token is provided in `secrets` it will be used to authenticate
     as that user.
-
-    This function uses the GitHub API and so has to make one request
-    per file. This means it is (a) slow and (b) uses up a lot of API
-    request quota. Creating a `pull_git` would solve these issues and
-    could be used for other Git hosting services.
     """
     assert source.get("repo"), "GitHub source must have a repo"
 
@@ -50,55 +45,53 @@ def pull_github(
         # Unauthenticated access
         client = Github()
 
+    path = path or "."
+
+    # Get the possibly token protected link for the repo archive
+    # See https://developer.github.com/v3/repos/contents/#download-a-repository-archive
     repo_resource = client.get_repo(source["repo"])
-    contents = repo_resource.get_contents(subpath)
-    if type(contents) is list:
-        return pull_directory(repo_resource, contents, path or "")
-    else:
-        return pull_file(repo_resource, contents, path or os.path.basename(subpath))
+    archive_link = repo_resource.get_archive_link("zipball")
 
+    # Get the archive. To avoid it filling up memory, stream directly to file
+    zip_file = tempfile.NamedTemporaryFile(suffix=".zip", delete=False)
+    with httpx.stream("GET", archive_link) as response:
+        for data in response.iter_bytes():
+            zip_file.write(data)
+    zip_file.close()
 
-def pull_file(repo_resource, contents, path: str) -> Files:
-    """
-    Pull a file from GitHub.
+    # Extract the desired subpath (if any) from the zip file
+    with ZipFile(zip_file.name, "r") as zip_archive:  #
+        files = {}
+        for zip_path in zip_archive.namelist():
+            # Skip directories
+            if zip_path[-1] == "/":
+                continue
 
-    For files over 1MB it is necessary to use a different API.
-    See https://docs.github.com/rest/reference/repos#get-repository-content.
-    """
-    if os.path.exists(path) and os.path.isdir(path):
-        shutil.rmtree(path)
+            # Remove the first element of the path (the repo name + hash)
+            inner_path = os.path.join(*(zip_path.split("/")[1:]))
 
-    if contents.size <= 1e6:
-        content = contents.content
-    else:
-        content = repo_resource.get_git_blob(contents.sha).content
+            # Save if in the subpath
+            remainder_path = None
+            if subpath == "":
+                remainder_path = inner_path
+            elif inner_path.startswith(subpath + "/"):
+                chars = len(subpath) + 1
+                remainder_path = inner_path[chars:]
+            elif inner_path == subpath:
+                remainder_path = inner_path
 
-    with open(path, "wb") as file:
-        shutil.copyfileobj(BytesIO(base64.b64decode(content)), file)  # type: ignore
+            if remainder_path:
+                data = zip_archive.read(zip_path)
 
-    return {path: file_info(path)}
+                dest_path = (
+                    os.path.join(path, remainder_path) if remainder_path else path
+                )
+                Path(dest_path).parent.mkdir(parents=True, exist_ok=True)
 
+                dest_file = open(dest_path, "wb")
+                dest_file.write(data)
+                dest_file.close()
 
-def pull_directory(repo_resource, contents_or_subpath, path: str) -> Files:
-    """
-    Pull a directory from GitHub.
-    """
-    if len(path):
-        if os.path.exists(path):
-            if not os.path.isdir(path):
-                os.unlink(path)
-        else:
-            os.makedirs(path, exist_ok=True)
+                files[remainder_path] = file_info(dest_path)
 
-    files = {}
-    for child in (
-        repo_resource.get_contents(contents_or_subpath)
-        if type(contents_or_subpath) is str
-        else contents_or_subpath
-    ):
-        child_path = os.path.join(path, child.name)
-        if child.type == "dir":
-            files.update(pull_directory(repo_resource, child.path, child_path))
-        else:
-            files.update(pull_file(repo_resource, child, child_path))
     return files
--- worker/jobs/pull/github_test.py
+++ worker/jobs/pull/github_test.py
@@ -1,4 +1,6 @@
 import os
+from contextlib import ContextDecorator
+from unittest import mock
 
 import pytest
 
@@ -7,34 +9,67 @@ from util.working_directory import working_directory
 from .github import pull_github
 
 
+def mocked_httpx_stream(*args, **kwargs):
+    """
+    VCR does not like recording HTTPX stream requests so mock it.
+
+    Returns a context manager that mimics that returned by
+    httpx.stream for the stencila/test repo.
+    """
+    return MockedHttpxStreamContext()
+
+
+class MockedHttpxStreamContext(ContextDecorator):
+    def __enter__(self, *args, **kwargs):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        return self
+
+    def iter_bytes(self):
+        return open(
+            os.path.join(
+                os.path.dirname(__file__), "cassettes", "github_test", "archive.zip"
+            ),
+            "rb",
+        )
+
+
 @pytest.mark.vcr
-def test_public_repo(tempdir):
+@mock.patch("httpx.stream", side_effect=mocked_httpx_stream)
+def test_public_repo(mock_stream, tempdir):
     with working_directory(tempdir.path):
-        pull_github(source=dict(repo="stencila/test", subpath="sub"))
+        files = pull_github(source=dict(repo="stencila/test", subpath="sub"))
+
+    assert list(files.keys()) == ["README.md"]
 
     tempdir.compare(["README.md"])
 
 
 @pytest.mark.vcr
-def test_subdirectory(tempdir):
+@mock.patch("httpx.stream", side_effect=mocked_httpx_stream)
+def test_subdirectory(mock_stream, tempdir):
     path = "a/very/very/deep/sub/directory"
 
     with working_directory(tempdir.path):
-        pull_github(
-            source=dict(repo="stencila/test", subpath=""), path=path,
-        )
+        files = pull_github(source=dict(repo="stencila/test", subpath=""), path=path,)
 
-    for expected in (
+    filenames = [
         ".travis.yml",
         "README.md",
         "document.md",
         "sub/README.md",
-    ):
+    ]
+
+    assert sorted(list(files.keys())) == sorted(filenames)
+
+    for expected in filenames:
         assert os.path.exists(os.path.join(tempdir.path, path, expected))
 
 
 @pytest.mark.vcr
-def test_single_file(tempdir):
+@mock.patch("httpx.stream", side_effect=mocked_httpx_stream)
+def test_single_file(mock_stream, tempdir):
     with working_directory(tempdir.path):
         pull_github(
             source=dict(repo="stencila/test", subpath="sub/README.md"),
