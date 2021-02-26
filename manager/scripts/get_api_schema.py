f67790d0b84050a6a0db848ac4c64cd3aa5084b7
Nokome Bentley <nokome@stenci.la>
2021-02-26 14:37:28 +1300

    chore(Scripts): Formatting

--- manager/scripts/get_api_schema.py
+++ manager/scripts/get_api_schema.py
@@ -2,7 +2,8 @@ from django.test import RequestFactory
 
 from manager.api.views.docs import schema_view
 
-def run (*args):
-    response = schema_view(RequestFactory().get('/api/schema'))
+
+def run(*args):
+    response = schema_view(RequestFactory().get("/api/schema"))
     response.render()
-    print(response.content.decode('utf-8'))
+    print(response.content.decode("utf-8"))
