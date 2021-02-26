8bb970616f605f2e60e8ab29898eef763e954a22
Nokome Bentley <nokome@stenci.la>
2021-02-26 16:37:43 +1300

    ci(Release): Avoid messaged in YAML

--- manager/scripts/get_api_schema.py
+++ manager/scripts/get_api_schema.py
@@ -3,7 +3,8 @@ from django.test import RequestFactory
 from manager.api.views.docs import schema_view
 
 
-def run(*args):
+def run(to):
     response = schema_view(RequestFactory().get("/api/schema"))
     response.render()
-    print(response.content.decode("utf-8"))
+    with open(to, "wb") as file:
+        file.write(response.content)
